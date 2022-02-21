099cab5c411f82b80663f0ccd21d9d653ee8f2e6
Juan Sáez García <contacto@juamber.com>
2022-02-21 21:53:17 +0100

    *ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
     name: 'Nicolas',
     age:18
   }
+  public names: String[] = ['Nico','July','Santi']
 
   public toggleButton(){
     this.btnDisabled = !this.btnDisabled;
