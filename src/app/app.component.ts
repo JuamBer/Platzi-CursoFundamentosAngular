e299a12f09cfb1d2073f6ccfd4b307938d0d258a
Juan Sáez García <contacto@juamber.com>
2022-02-21 16:48:16 +0100

    string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  public name = 'Nicolas';
+  public age = 18;
 }
