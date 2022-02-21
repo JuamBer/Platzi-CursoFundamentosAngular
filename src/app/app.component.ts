275952f42d3e1b02f3079973f6a13a666761555b
Juan Sáez García <contacto@juamber.com>
2022-02-21 16:57:49 +0100

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public name = 'Nicolas';
-  public age = 18;
+  public name: String = 'Nicolas';
+  public age: Number = 18;
+  public btnDisabled: Boolean = true;
+  public img: String = 'https://multimedia.comunitatvalenciana.com/A1761560812F4470B6A1D8BC96835C8D/img/73A120AB817648A7B990AB66E8FB824D/JUCAR-0011.jpg?responsive'
+  public person = {
+    name: 'Nicolas',
+    age:18
+  }
 }
