470fbfd6186af188004200de97502db956bdb5de
Juan Sáez García <contacto@juamber.com>
2022-02-21 17:11:29 +0100

    otros eventos que puedes escuchar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,4 +21,14 @@ export class AppComponent {
   public increaseAge(){
     this.person.age += 1;
   }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
